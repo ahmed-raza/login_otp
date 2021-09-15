02d45a2cabe0aa7eddfc1574b53059a3dcff05bb
Alex Roye <alex@creativefaze.com>
2021-09-15 17:07:04 +0500

    Generating and sending OTP

--- src/OTP.php
+++ src/OTP.php
@@ -2,25 +2,27 @@
 
 namespace Drupal\login_otp;
 
-use Drupal\Core\Password\PasswordInterface;
-
 class OTP {
   private $username;
-  private $otp;
+  private $tempStorageFactory;
 
-  public function __construct($username) {
-    $this->username = $username;
+  public function __construct() {
+    $this->tempStorageFactory = \Drupal::service('tempstore.private');
   }
 
-  public function generateOTP() {
-    $getUid = $this->getUid();
+  public function generateOTP($username) {
+    $this->username = $username;
 
+    $uid = $this->getField('uid');
     $human_readable_otp = rand(100000, 999999);
+
+    $this->tempStorageFactory->get('login_otp')->set('uid', $uid);
+    $this->tempStorageFactory->get('login_otp')->set('otp', $human_readable_otp);
 
     $database = \Drupal::database();
     $database = $database->insert('login_otp')->fields([
-      'uid' => $this->uid,
-      'otp' => PasswordInterface::hash($human_readable_otp),
+      'uid' => $uid,
+      'otp' => \Drupal::service('password')->hash($human_readable_otp),
       'expiration' => strtotime("+5 minutes",time())
     ]);
 
@@ -28,13 +30,26 @@ class OTP {
       return $human_readable_otp;
     }
 
-    return $getUid;
+    return FALSE;
   }
 
-  private function getUid() {
+  public function sendOTP($otp) {
+    $mail_manager = \Drupal::service('plugin.manager.mail');
+
+    $to = $this->getField('mail');
+    $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    $params['message'] = $this->t('Hello, @username <br> This is the OTP you will use to login: @otp',
+                        [
+                          '@username' => $this->username,
+                          '@otp' => $otp
+                        ]);
+    return $mail_manager->mail('login_otp', 'login_otp_mail', $to, $langcode, $params, NULL, TRUE);
+  }
+
+  private function getField($field) {
     $database = \Drupal::database();
     $query = $database->select('users_field_data', 'u')
-              ->fields('u', ['uid'])
+              ->fields('u', [$field])
               ->condition('name', $this->username, '=')
               ->execute()
               ->fetchAssoc();
