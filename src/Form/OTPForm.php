bf91f4bfd38c3f5b3dc1c719e41609450f81b8f4
Alex Roye <alex@creativefaze.com>
2021-09-15 18:07:47 +0500

    Validating OTP and authenticating user

--- src/Form/OTPForm.php
+++ src/Form/OTPForm.php
@@ -7,7 +7,9 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\HtmlCommand;
 use Drupal\Core\Ajax\ReplaceCommand;
-use Drupal\Core\Ajax\CssCommand;
+use Drupal\Core\Ajax\RedirectCommand;
+use Drupal\login_otp\OTP;
+use Drupal\user\Entity\User;
 
 /**
  * Class OTPForm.
@@ -47,12 +49,19 @@ class OTPForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    $otp = new OTP();
+    $uid = \Drupal::service('tempstore.private')->get('login_otp')->get('uid');
     foreach ($form_state->getValues() as $key => $value) {
-      if ($key == 'otp' && empty($value)) {
-        $form_state->setErrorByName('otp', $this->t('OTP is required.'));
-      }
-      if ($key == 'otp' && strlen($value) < 6) {
-        $form_state->setErrorByName('otp', $this->t('OTP is incomplete.'));
+      if ($key == 'otp') {
+        if (empty($value)) {
+          $form_state->setErrorByName('otp', $this->t('OTP is required.'));
+        }
+        if (strlen($value) < 6) {
+          $form_state->setErrorByName('otp', $this->t('OTP is incomplete.'));
+        }
+        if ($otp->check($uid, $value) == false) {
+          $form_state->setErrorByName('otp', $this->t('Invalid or expired OTP.'));
+        }
       }
     }
     parent::validateForm($form, $form_state);
@@ -70,18 +79,14 @@ class OTPForm extends FormBase {
       $response->addCommand(new ReplaceCommand('.otp-form', $form));
       return $response;
     }
-    \Drupal::messenger()
-      ->addMessage($this->t('@value', [
-        '@value' => $form_state->getValue('otp')
-      ]));
-    unset($form['#prefix']);
-    unset($form['#suffix']);
-    $form['status_messages'] = [
-      '#type'   => 'status_messages',
-      '#weight' => -10,
-    ];
-    $form['otp']['#value'] = '';
-    $response->addCommand(new ReplaceCommand('.otp-form', $form));
+    $tempstore = \Drupal::service('tempstore.private')->get('login_otp');
+    $account = User::load($tempstore->get('uid'));
+    $otp = new OTP();
+    $otp->expire($tempstore->get('uid'));
+    $tempstore->delete('email');
+    user_login_finalize($account);
+    $redirect_command = new RedirectCommand('/user');
+    $response->addCommand($redirect_command);
     return $response;
   }
 
